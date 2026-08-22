d5a60be71d7a089ff66ab006082271644577a09a
NodeNexus <dev@nodenexus.io>
2026-08-22 23:30:01 +0530

    Implement point containment logic in Rect

--- src/utils/Geometry.h
+++ src/utils/Geometry.h
@@ -18,3 +18,4 @@ namespace geom {
     Rect() : x(0), y(0), w(0), h(0) {}
     Rect(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
     bool contains(const Point& p) const {
+      return p.x >= x && p.x < x + w && p.y >= y && p.y < y + h;
